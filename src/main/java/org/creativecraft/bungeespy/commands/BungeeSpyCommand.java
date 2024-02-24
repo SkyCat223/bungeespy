b030f1517e7388925fd0e7d2bd1e3596f59ad890
SkyCat223 <158812891+SkyCat223@users.noreply.github.com>
2024-02-24 09:42:43 +0100

    Update BungeeSpyCommand.java

--- src/main/java/org/creativecraft/bungeespy/commands/BungeeSpyCommand.java
+++ src/main/java/org/creativecraft/bungeespy/commands/BungeeSpyCommand.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.UUID;
 
-@CommandAlias("bungeespy")
+@CommandAlias("bspy")
 @Description("Spy on player commands network-wide.")
 public final class BungeeSpyCommand extends BaseCommand implements Listener {
     @Dependency
